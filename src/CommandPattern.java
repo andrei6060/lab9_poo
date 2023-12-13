3d7ec3fc16123e8e8571c6ac6ae1bf1ca9353fbd
Constantinescu Andrei <andrei.iunie2002@gmail.com>
2023-12-13 23:51:08 +0200

    undo done for command pattern

--- src/CommandPattern.java
+++ src/CommandPattern.java
@@ -1,23 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Interfața pentru comenzi
 interface Command {
     void execute();
+    void undo();
 }
 
 // Clase care implementează o comandă specifică
 class LightOnCommand implements Command {
     private Light light;
+    private boolean previousState;
+
     public LightOnCommand(Light light) {
         this.light = light;
     }
 
     @Override
     public void execute() {
+        previousState = light.getState();
         light.turnOn();
+    }
+
+    @Override
+    public void undo() {
+        if (!previousState) {
+            light.turnOff();
+        }
     }
 }
 
 class LightOffCommand implements Command {
     private Light light;
+    private boolean previousState;
 
     public LightOffCommand(Light light) {
         this.light = light;
@@ -25,24 +40,42 @@ class LightOffCommand implements Command {
 
     @Override
     public void execute() {
+        previousState = light.getState();
         light.turnOff();
+    }
+
+    @Override
+    public void undo() {
+        if (previousState) {
+            light.turnOn();
+        }
     }
 }
 
 // Clasa de lumină care va fi controlată de comenzile de lumină
 class Light {
+    private boolean state = false;
+
     public void turnOn() {
+        state = true;
         System.out.println("Light is ON");
     }
 
     public void turnOff() {
+        state = false;
         System.out.println("Light is OFF");
+    }
+
+    public boolean getState() {
+        return state;
     }
 }
 
 // Clasa invocator care primește și execută comenzile
 class RemoteControl {
     private Command command;
+    private List<Command> commandHistory = new ArrayList<>();
+    private int currentCommandIndex = -1;
 
     public void setCommand(Command command) {
         this.command = command;
@@ -50,6 +83,16 @@ class RemoteControl {
 
     public void pressButton() {
         command.execute();
+        commandHistory.add(command);
+        currentCommandIndex++;
+    }
+
+    public void undo() {
+        if (currentCommandIndex >= 0) {
+            Command lastCommand = commandHistory.get(currentCommandIndex);
+            lastCommand.undo();
+            currentCommandIndex--;
+        }
     }
 }
 
@@ -60,7 +103,7 @@ public class CommandPattern {
         Command lightOnCommand = new LightOnCommand(light);
         Command lightOffCommand = new LightOffCommand(light);
 
-        //invocator
+        // Invocator
         RemoteControl remoteControl = new RemoteControl();
 
         remoteControl.setCommand(lightOnCommand);
@@ -68,5 +111,8 @@ public class CommandPattern {
 
         remoteControl.setCommand(lightOffCommand);
         remoteControl.pressButton();
+
+        // Undo
+        remoteControl.undo();
     }
 }
